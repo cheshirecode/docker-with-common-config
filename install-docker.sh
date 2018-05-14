befc8e113b295b13305d09378956655ee88492ea
Tran Dac Anh <fred@cxagroup.com>
2018-05-14 18:00:39 +0800

    update Docker installation script

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-sudo apt-get remove docker docker-engine docker.io docker-ce;
-sudo apt-get install -y \
+sudo apt-get purge -y docker-engine docker docker.io docker-ce
+sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
+sudo rm -rf /var/lib/docker
+sudo rm /etc/apparmor.d/docker
+sudo groupdel docker
+sudo rm -rf /var/run/docker.sock
+sudo apt-get update;
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,9 +16,9 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable";
-sudo apt-get update;
-sudo apt-get install -y docker-ce=17.12.0~ce-0~ubuntu;
+sudo apt-get -y install docker-ce=17.12.0~ce-0~ubuntu
+sudo usermod -aG docker `whoami`
 # enable swap resource limiting
 sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT="\)"/\1cgroup_enable=memory swapaccount=1"/' /etc/default/grub;
 sudo update-grub;
-sudo apt autoremove;
+sudo apt autoremove -y;
